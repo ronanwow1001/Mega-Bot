81bf1d17548c8edc6cd2371792a46dbeb75c1e64
Turntablelover <darthvader438@gmail.com>
2013-12-30 14:27:45 -0500

    Bot Code

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ Mega-Bot
 ========
 
 Plug.DJ Bot
+
+Version Number: 1.0.1
+
+Debut Date: December 10, 2013
+ 
+Features:
+1. Welcome Greeting
+ 
+Release Notes:
+ 
+December 10, 2013:
+ 
+1.0.0:
+1. Initial release on Plug.DJ
+ 
+December 14, 2013:
+ 
+1.0.1:
+1. Welcome Greeting
+
+
---
+++ bot.js
@@ -0,0 +1,22 @@
+var PlugAPI = require('./plugapi');
+var ROOM = 'christian-anything-2';
+var UPDATECODE = '$&2h72=^^@jdBf_n!`-38UHs'; 
+
+// Instead of providing the AUTH, you can use this static method to get the AUTH cookie via twitter login credentials:
+PlugAPI.getAuth({
+    username: 'PlugDJBot2',
+    password: 'Starwarskotor1'
+}, function(err, auth) { // if err is defined, an error occurred, most likely incorrect login
+    if(err) {
+        console.log("An error occurred: " + err);
+        return;
+    }
+    var bot = new PlugAPI(auth, UPDATECODE);
+	
+    bot.connect(ROOM);
+
+    bot.on('roomJoin', function(data) {
+        // data object has information on the room - list of users, song currently playing, etc.
+        console.log("Joined " + ROOM + ": ", data);
+    });
+});
